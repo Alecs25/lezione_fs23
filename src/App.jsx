9826cdaebd0dac5d070d6e2c014a2d27cadbdd92
Alecs25 <alex00romagnoli@gmail.com>
2024-03-27 20:04:15 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-import { useState } from "react";
-import { Todo } from "./Todo";
+//L'obiettivo è quello di creare una ToDo List che abbia le funzioni di: Aggiunta, rimozione per ogni task generata.
+//E che preveda un campo input per personalizzare il nome della task.
+//Utilizzare un componente TODO per renderizzare la task.
 
 function App() {
-	const [todo, setTodo] = useState([]);
-	const [input, setInput] = useState("");
-
-	function handleInput(e) {
-		setInput(e.target.value);
-	}
-
-	function handleClick() {
-		setTodo([...todo, input]);
-	}
-
-	function handleDelete(index) {
-		const result = todo.filter((e, i) => i !== index);
-		setTodo(result);
-	}
-
-	return (
-		<>
-			<input value={input} onChange={handleInput} />
-			<button onClick={handleClick}>Add task</button>
-			<ul>
-				{todo &&
-					todo.map((e, i) => {
-						return (
-							<Todo id={i} callback={handleDelete}
-              taskName={e}></Todo>
-						);
-					})}
-			</ul>
-		</>
-	);
+	return <></>;
 }
 
 export default App;
--- src/Todo.jsx
+++
@@ -1,8 +0,0 @@
-export function Todo({ id, taskName, callback}) {
-	return (
-		<li key={id}>
-			{taskName}
-			<button callback={callback} onClick={() => callback(id)}>Delete task</button>
-		</li>
-	);
-}
